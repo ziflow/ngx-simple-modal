ffb6d66e0c97a5c6b9c02241dae8591b9f3850ec
Pawel Czekaj <pawel.czekaj@ziflow.com>
2023-05-25 10:17:44 +0200

    release: 16.0.0-dev.1

--- scripts/release.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-
-try {
-  fs.copy('src/styles', 'dist/styles');
-  console.log('Copied additional files');
-} catch (e) {
-  console.error(`Failed to copy additional files due to: ${e}`);
-}
